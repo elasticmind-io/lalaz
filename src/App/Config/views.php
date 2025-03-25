804ed5591968e21717771d1c74daaee59b4ee564
Gregory Serrao <gregperes84@gmail.com>
2025-03-25 19:28:46 -0400

    feat: view extensions support

--- src/App/Config/views.php
+++ src/App/Config/views.php
@@ -8,5 +8,23 @@ return [
     'provider' => env('TEMPLATE_PROVIDER', TwigTemplateEngine::class),
 
     // Define the views path of the app
-    'path' => env('VIEWS_PATH', '/Views')
+    'path' => env('VIEWS_PATH', '/Views'),
+
+    /**
+     * PHP files that receive the Twig Environment instance and allow the developer
+     * to extend the templating system by registering custom functions, filters,
+     * or global variables.
+     *
+     * Each file must return a callable in the following format:
+     * fn(Twig\Environment $twig): void
+     *
+     * Example:
+     * return function (\Twig\Environment $twig) {
+     *     $twig->addFunction(new \Twig\TwigFunction('app_name', fn() => 'My App'));
+     *     $twig->addGlobal('now', date('Y-m-d H:i:s'));
+     * };
+     */
+    'extensions' => env('VIEW_EXTENSIONS', [
+        //'./src/App/Support/Extensions/views.php'
+    ])
 ];
